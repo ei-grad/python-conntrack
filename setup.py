179f2f9e91412ac79ccf0aaf34762b6d63ed8c39
Andrew S. Grigorev <andrew@ei-grad.ru>
2010-01-21 03:01:14 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Conntrack',
+    version='0.1',
+    description='A simple python interface to libnetfilter_conntrack using ctypes.',
+    author='Andrew Grigorev',
+    author_email='andrew@ei-grad.ru',
+    url='http://github.com/ei-grad/python-conntrack',
+    py_modules=['Conntrack'],
+
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: System :: Networking :: Monitoring',
+        ),
+
+    license="MIT"
+)
